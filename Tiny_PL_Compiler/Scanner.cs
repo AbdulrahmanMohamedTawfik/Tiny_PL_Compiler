b1525bd7c07eae6880e05f32d1fbfe2e2707d906
AbdulrahmanMohamedTawfik <20201701098@cis.asu.edu.eg>
2022-11-20 17:15:01 +0200

    tokens updated

--- Tiny_PL_Compiler/Scanner.cs
+++ Tiny_PL_Compiler/Scanner.cs
@@ -6,11 +6,9 @@ using System.Threading.Tasks;
 
 public enum Token_Class
 {
-    Begin, Call, Declare, End, Do, Else, EndIf, EndUntil, EndWhile, If, Integer,
-    Parameters, Procedure, Program, Read, Real, Set, Then, Until, While, Write,
-    Dot, Semicolon, Comma, LParanthesis, RParanthesis, EqualOp, LessThanOp,
-    GreaterThanOp, NotEqualOp, PlusOp, MinusOp, MultiplyOp, DivideOp,
-    Idenifier, Constant
+    Int, Float, String, Read, Write, Repeat, Until, If, Elseif, Else, Then, Return,
+    Endl, Parameters, Semicolon, Comma, LParanthesis, RParanthesis, Lbrace, Rbrace, EqualOp, LessThanOp,
+    GreaterThanOp, NotEqualOp, PlusOp, MinusOp, MultiplyOp, DivideOp, AndOp,OrOp, Idenifier, Constant
 }
 namespace TinyCompiler
 {
@@ -30,44 +28,37 @@ namespace TinyCompiler
 
         public Scanner()
         {
-            ReservedWords.Add("if", Token_Class.If);
-            ReservedWords.Add("begin", Token_Class.Begin);
-            ReservedWords.Add("call", Token_Class.Call);
-            ReservedWords.Add("declare", Token_Class.Declare);
-            ReservedWords.Add("end", Token_Class.End);
-            ReservedWords.Add("do", Token_Class.Do);
-            ReservedWords.Add("else", Token_Class.Else);
-            ReservedWords.Add("endif", Token_Class.EndIf);
-            ReservedWords.Add("enduntil", Token_Class.EndUntil);
-            ReservedWords.Add("endwhile", Token_Class.EndWhile);
-            ReservedWords.Add("integer", Token_Class.Integer);
-            ReservedWords.Add("parameters", Token_Class.Parameters);
-            ReservedWords.Add("procedure", Token_Class.Procedure);
-            ReservedWords.Add("program", Token_Class.Program);
+            ReservedWords.Add("int", Token_Class.Int);
+            ReservedWords.Add("float", Token_Class.Float);
+            ReservedWords.Add("string", Token_Class.String);
             ReservedWords.Add("read", Token_Class.Read);
-            ReservedWords.Add("real", Token_Class.Real);
-            ReservedWords.Add("set", Token_Class.Set);
-            ReservedWords.Add("then", Token_Class.Then);
-            ReservedWords.Add("until", Token_Class.Until);
-            ReservedWords.Add("while", Token_Class.While);
             ReservedWords.Add("write", Token_Class.Write);
+            ReservedWords.Add("repeat", Token_Class.Repeat);
+            ReservedWords.Add("until", Token_Class.Until);
+            ReservedWords.Add("if", Token_Class.If);
+            ReservedWords.Add("else", Token_Class.Else);
+            ReservedWords.Add("elseif", Token_Class.Elseif);
+            ReservedWords.Add("then", Token_Class.Then);
+            ReservedWords.Add("return", Token_Class.Return);
+            ReservedWords.Add("endl", Token_Class.Endl);
+            ReservedWords.Add("parameters", Token_Class.Parameters);
 
-            Operators.Add(".", Token_Class.Dot);
             Operators.Add(";", Token_Class.Semicolon);
             Operators.Add(",", Token_Class.Comma);
             Operators.Add("(", Token_Class.LParanthesis);
             Operators.Add(")", Token_Class.RParanthesis);
+            Operators.Add("{", Token_Class.Lbrace);
+            Operators.Add("}", Token_Class.Rbrace);
             Operators.Add("=", Token_Class.EqualOp);
             Operators.Add("<", Token_Class.LessThanOp);
             Operators.Add(">", Token_Class.GreaterThanOp);
-            Operators.Add("!", Token_Class.NotEqualOp);
+            Operators.Add("<>", Token_Class.NotEqualOp);
             Operators.Add("+", Token_Class.PlusOp);
             Operators.Add("-", Token_Class.MinusOp);
             Operators.Add("*", Token_Class.MultiplyOp);
             Operators.Add("/", Token_Class.DivideOp);
-
-
-
+            Operators.Add("||", Token_Class.OrOp);
+            Operators.Add("&&", Token_Class.AndOp);
         }
 
         public void StartScanning(string SourceCode)
@@ -178,7 +169,7 @@ namespace TinyCompiler
 
             else
             {
-                Errors.Error_List.Add("Unidentified Token \'" + Lex+"\'");
+                Errors.Error_List.Add("Unidentified Token \'" + Lex + "\'");
             }
 
         }
